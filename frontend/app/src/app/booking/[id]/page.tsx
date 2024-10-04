c4aeae254324ab6fb8a04602326c4f423a1131a5
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-10-04 13:46:42 +0200

    "Add Error Handling and No Data Message"

--- frontend/app/src/app/booking/[id]/page.tsx
+++ frontend/app/src/app/booking/[id]/page.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
 import { getBookingsById } from '@/app/hooks/use-request';
+import { Typography } from '@mui/material';
 
 const BookingDetailPage: React.FC = async ({ params }) => {
     const { id } = params;
     const { data, error } = await getBookingsById(parseInt(id));
+
+    if (error) {
+        return <p style={{ color: "red", textAlign:"center", marginTop: "20px", fontSize: "25px" }}>Error: {error}</p>;
+    }
+
+    if (!data) {
+        return <Typography>No bookings found.</Typography>;
+    }
 
     return (
         <div>Booking Detail Page Placeholder</div>
