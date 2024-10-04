e8d9e19f2a2ff1e98b690bd9b9c8eb6ca7fe4524
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-10-04 13:45:59 +0200

    "Initial Setup of BookingDetailPage Component"

---
+++ frontend/app/src/app/booking/[id]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const BookingDetailPage: React.FC = () => {
+    return (
+        <div>Booking Detail Page Placeholder</div>
+    );
+};
+
+export default BookingDetailPage;
