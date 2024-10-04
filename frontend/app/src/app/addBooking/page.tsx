232d57bab88671b03182f77fae422d4b88996ba3
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-10-04 13:49:29 +0200

    "Add Form State Management for New Booking"

--- frontend/app/src/app/addBooking/page.tsx
+++ frontend/app/src/app/addBooking/page.tsx
@@ -1,11 +1,28 @@
 "use client"
 import React, { useState } from 'react';
 import { Box, Typography } from '@mui/material';
+import { BookingData } from '../types/bookingData';
 
 const NewBooking: React.FC = () => {
+  const [form, setForm] = useState<BookingData>({
+    service: '',
+    doctor_name: '',
+    date: '',
+    start_time: '',
+    end_time: ''
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+    const { name, value } = e.target;
+    setForm((prevForm) => ({
+      ...prevForm,
+      [name]: value,
+    }));
+  };
+
   return (
     <Box>
-      <Typography>New Booking Placeholder</Typography>
+      <Typography>New Booking Form</Typography>
     </Box>
   );
 };
