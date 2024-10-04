2b08f9f7e9cb1465a270f3a82b38f7f38279c2b5
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-10-04 13:49:54 +0200

    "Add Basic Form Structure with MUI Components"

--- frontend/app/src/app/addBooking/page.tsx
+++ frontend/app/src/app/addBooking/page.tsx
@@ -1,7 +1,7 @@
 "use client"
 import React, { useState } from 'react';
-import { Box, Typography } from '@mui/material';
 import { BookingData } from '../types/bookingData';
+import { Box, Button, TextField, Typography } from '@mui/material';
 
 const NewBooking: React.FC = () => {
   const [form, setForm] = useState<BookingData>({
@@ -21,8 +21,18 @@ const NewBooking: React.FC = () => {
   };
 
   return (
-    <Box>
-      <Typography>New Booking Form</Typography>
+    <Box sx={{ maxWidth: 400, margin: '0 auto', padding: 2 }}>
+      <form>
+        <Typography sx={{ color: "gray", fontSize: "2rem", textAlign: "center" }}>Add a Booking</Typography>
+        <TextField label="Doctor Name" name="doctor_name" value={form.doctor_name} onChange={handleChange} fullWidth required margin="normal" />
+        <TextField label="Service" name="service" value={form.service} onChange={handleChange} fullWidth required margin="normal" />
+        <TextField label="Date" type="date" name="date" value={form.date} onChange={handleChange} fullWidth required margin="normal" InputLabelProps={{ shrink: true, }} />
+        <TextField label="Start Time" type="time" name="start_time" value={form.start_time} onChange={handleChange} fullWidth required margin="normal" InputLabelProps={{ shrink: true, }} />
+        <TextField label="End Time" type="time" name="end_time" value={form.end_time} onChange={handleChange} fullWidth required margin="normal" InputLabelProps={{ shrink: true, }} />
+        <Button type="submit" sx={{ mt: 2, backgroundColor: "#488598 !important", maxWidth: "550px", color: "white" }}>
+          Add Booking
+        </Button>
+      </form>
     </Box>
   );
 };
