204f610d8c5452ac52896df9b39fa64e3d5a1652
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-10-04 13:49:04 +0200

    "Initial Setup of NewBooking Component"

---
+++ frontend/app/src/app/addBooking/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import React, { useState } from 'react';
+import { Box, Typography } from '@mui/material';
+
+const NewBooking: React.FC = () => {
+  return (
+    <Box>
+      <Typography>New Booking Placeholder</Typography>
+    </Box>
+  );
+};
+
+export default NewBooking;
