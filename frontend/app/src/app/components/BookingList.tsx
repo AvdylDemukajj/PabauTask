20c07f368a6779439b182fb8ffd2411002602230
AvdylDemukajj <avdyldemukaj38@gmail.com>
2024-10-04 13:35:37 +0200

    "Styling and MUI Enhancements"

--- frontend/app/src/app/components/BookingList.tsx
+++ frontend/app/src/app/components/BookingList.tsx
@@ -1,32 +1,34 @@
-import { Table, TableBody, TableCell, TableHead, TableRow, Paper, TableContainer } from '@mui/material';
+import { Typography, Box } from '@mui/material';
 
 const BookingList: React.FC<BookingListProps> = ({ bookings, error }) => {
   if (error) {
-    return <p>Error: {error}</p>;
+    return <Typography color="error">Error: {error}</Typography>;
   }
 
   if (bookings.length === 0) {
-    return <p>No bookings found.</p>;
+    return <Typography>No bookings found.</Typography>;
   }
 
   return (
-    <TableContainer component={Paper}>
-      <Table>
-        <TableHead>
-          <TableRow>
-            <TableCell>Booking</TableCell>
-            <TableCell>Details</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {bookings.map((booking) => (
-            <TableRow key={booking.id}>
-              <TableCell>Booking on {booking.date}</TableCell>
-              <TableCell>Details</TableCell>
+    <Box sx={{ maxWidth: '800px', margin: '0 auto', boxShadow: '0 6px 20px rgba(0, 0, 0, 0.1)' }}>
+      <TableContainer component={Paper} elevation={0}>
+        <Table>
+          <TableHead sx={{ backgroundColor: "#488598" }}>
+            <TableRow>
+              <TableCell sx={{ color: "white" }}>Booking</TableCell>
+              <TableCell sx={{ color: "white" }}>Details</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {bookings.map((booking) => (
+              <TableRow key={booking.id}>
+                <TableCell>A Booking on {booking.date}</TableCell>
+                <TableCell>Details</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
   );
 };
